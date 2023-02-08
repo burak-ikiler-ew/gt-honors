a3804503bae81f2181bc003c13a3ae000fc61518
Firat Faik Ikiler <firatfaik@gmail.com>
2023-02-08 11:55:55 -0800

    refs #44376: Implementing new library and fixes for composer.

---
+++ web/themes/custom/gt_honors/src/js/parvus.js
@@ -0,0 +1,24 @@
+/**
+ * Isolate the context in an IIFE
+ *
+ * @param object $ jQuery object
+ * @param object Drupal Drupal object
+ * @param window window Current window object
+ */
+
+// eslint-disable-next-line func-names
+(function($, Drupal) {
+  Drupal.behaviors.parvusJS = {
+    attach(context) {
+      $("body", context)
+        .once("parvusJS")
+        // eslint-disable-next-line func-names
+        .each(function() {
+          // eslint-disable-next-line no-undef
+          const prvs = new Parvus({
+            selector: ".lightbox"
+          });
+        });
+    }
+  };
+})(jQuery, Drupal);
