4842d5112efc44f6de0774a13598b54815f7683a
Iskandar Mamadnazarov <iskandar.mamadnazarov@evolvingweb.ca>
2023-04-18 16:35:57 +0300

    refs #45408: Mobile main navbar menu is themed.

--- web/themes/custom/gt_honors/patterns/organisms/header/js/header.js
+++ web/themes/custom/gt_honors/patterns/organisms/header/js/header.js
@@ -6,6 +6,7 @@
  * @param window window Current window object
  */
 
+/* eslint-disable no-use-before-define */
 (function ($, Drupal, window) {
   /**
    * Main script for the current theme
@@ -65,23 +66,110 @@
       const dropdown = $('header .dropdown');
 
       dropdown.each(function () {
-        const dropdownToggle = $(this).find('.dropdown-toggle');
+        const dropdownToggleMobile = $(this).find('.dropdown-toggle--mobile');
+        const dropdownToggleDesktop = $(this).find('.dropdown-toggle--desktop');
+
         const dropdownMenu = $(this).find('.dropdown-menu');
         const dropdownMenuItems = dropdownMenu.find('a');
-        const str = `dropdown-toggle__${dropdownToggle.attr('data-drupal-link-system-path')}`;
+
+        const str = `dropdown-toggle--${dropdownToggleDesktop.attr('data-unique-id')}`;
         const newId = str.replaceAll('/', '-');
-        const focusableItems = dropdownToggle.add(dropdownMenuItems);
-        dropdownToggle.attr('aria-haspopup', 'true');
-        dropdownToggle.attr('aria-expanded', 'false');
-        dropdownToggle.attr('id', newId);
-        dropdownMenu.attr('aria-labelledby', dropdownToggle.attr('id'));
+        const focusableItems = dropdownToggleDesktop.add(dropdownMenuItems);
+
+        dropdownToggleMobile.attr('aria-haspopup', 'true');
+        dropdownToggleMobile.attr('aria-expanded', 'false');
+        dropdownToggleMobile.attr('id', newId);
+
+        dropdownToggleDesktop.attr('aria-haspopup', 'true');
+        dropdownToggleDesktop.attr('aria-expanded', 'false');
+        dropdownToggleDesktop.attr('id', newId);
+
+        dropdownMenu.attr('aria-labelledby', dropdownToggleDesktop.attr('id'));
+
+        $(this).on('shown.bs.dropdown', () => {
+          $(this).find('.dropdown-menu').removeAttr('style');
+        });
 
         focusableItems.on('blur mouseout', () => {
-          dropdownToggle.attr('aria-expanded', 'false');
+          dropdownToggleDesktop.attr('aria-expanded', 'false');
         }).on('focus mouseover', () => {
-          dropdownToggle.attr('aria-expanded', 'true');
+          dropdownToggleDesktop.attr('aria-expanded', 'true');
         });
       });
+    },
+  };
+
+  Drupal.behaviors.headerMenuTogglers = {
+    attach (context) {
+      if (context !== document) {
+        return;
+      }
+
+      const body = context.querySelector('body');
+      const mobileMenu = context.getElementById('mainMenu');
+      const mobileMenuToggle = context.getElementById('mobileMenuToggle');
+
+      const isSearchBarExpanded = false; // for future use;
+      let isMobileMenuExpanded = false;
+
+      mobileMenuToggle.addEventListener('click', toggleMobileMenu);
+
+      function toggleMobileMenu () {
+        toggleMobileMenuPopup();
+        toggleBodyScroll();
+        toggleListeners();
+      }
+
+      function toggleMobileMenuPopup () {
+        const toggledClassName = 'primary-menu--expanded';
+
+        if (isMobileMenuExpanded) {
+          mobileMenu.classList.remove(toggledClassName);
+          mobileMenuToggle.setAttribute('aria-expanded', false);
+          isMobileMenuExpanded = false;
+        } else {
+          mobileMenu.classList.add(toggledClassName);
+          mobileMenuToggle.setAttribute('aria-expanded', true);
+          isMobileMenuExpanded = true;
+        }
+      }
+
+      function toggleBodyScroll () {
+        const toggledClassName = 'noscroll';
+
+        if (isMobileMenuExpanded || isSearchBarExpanded) {
+          body.classList.add(toggledClassName);
+        } else {
+          body.classList.remove(toggledClassName);
+        }
+      }
+
+      function toggleListeners () {
+        if (isMobileMenuExpanded) {
+          body.addEventListener('focusin', collapseMobilePopupOnFocusOut);
+          context.addEventListener('keydown', collaspseMobilePopupOnEscapeKey);
+        } else {
+          body.removeEventListener('focusin', collapseMobilePopupOnFocusOut);
+          context.removeEventListener('keydown', collaspseMobilePopupOnEscapeKey);
+        }
+      }
+
+      function collapseMobilePopupOnFocusOut () {
+        const { activeElement } = context;
+        const isFocusOutsideMenu = !mobileMenu.contains(activeElement);
+
+        if (isMobileMenuExpanded && isFocusOutsideMenu && (activeElement !== mobileMenuToggle)) {
+          toggleMobileMenu();
+        }
+      }
+
+      function collaspseMobilePopupOnEscapeKey (event) {
+        const isEscapeBtnPressed = event.key === 'Escape';
+
+        if (isMobileMenuExpanded && isEscapeBtnPressed) {
+          toggleMobileMenu();
+        }
+      }
     },
   };
 }(jQuery, Drupal, window));
